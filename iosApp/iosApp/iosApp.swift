2c7dbc7b04e8ebf80fb91e0af7caff51ce31dab0
info <aspigon1@gmail.com>
2026-08-17 19:40:35 +0200

    Fix: Move iOS init logic to Kotlin and simplify Swift code

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -3,12 +3,6 @@ import Shared
 
 @main
 struct iosApp: App {
-    init() {
-        // Initialize Database and Repositories on iOS
-        let db = BibleDatabaseKt.getDatabaseBuilder().build()
-        BibleRepository.shared.initializeDatabase(db: db)
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
--- shared/src/iosMain/kotlin/com/test/myapplication/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/test/myapplication/MainViewController.kt
@@ -1,7 +1,18 @@
 package com.test.myapplication
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.ComposeUIViewController
+import com.test.myapplication.data.local.BibleDatabaseProvider
+import com.test.myapplication.data.local.getDatabaseBuilder
+import com.test.myapplication.data.repository.BibleRepository
 import com.test.myapplication.ui.App
 import platform.UIKit.UIViewController
 
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController = ComposeUIViewController {
+    LaunchedEffect(Unit) {
+        val db = BibleDatabaseProvider.getDatabase(getDatabaseBuilder())
+        BibleRepository.initializeDatabase(db)
+        BibleRepository.ensureSeeded()
+    }
+    App()
+}
