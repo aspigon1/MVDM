3cce814289f76e5e724d0d440290491f84a57cd2
info <aspigon1@gmail.com>
2026-08-17 19:12:48 +0200

    Fix: Restore Compose compiler plugin and disable config cache in CI

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     id("com.android.library")
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
